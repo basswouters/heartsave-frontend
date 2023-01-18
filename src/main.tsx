37bd8964646921bff65fab10481e891b781a725b
Bas Wouters <bas@idreamz.nl>
2023-01-18 17:17:51 +0100

    staging test

--- src/main.tsx
+++ src/main.tsx
@@ -5,5 +5,6 @@ import ReactDOM from 'react-dom/client';
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <AuthenticatedApp />
+    asd
   </React.StrictMode>,
 );
